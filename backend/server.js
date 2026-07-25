33883c2e81f9666c96361378315f98bb72743646
Kartik Garg <kartikamitgarg2005@gmail.com>
2026-07-25 21:08:32 +0530

    feat: configure CORS middleware to allow requests from specified client URL

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import { errorHandler } from "./error.middleware.js";
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173" }));
 app.use(express.json());
 
 // Mount Routes
